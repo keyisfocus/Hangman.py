f5b9ce4335849134f4f24d3e3f1b4ad41c299c12
keyisfocus <keyisfocus@gmail.com>
2021-03-24 17:15:37 +0300

    Stage 6

--- hangman.py
+++ hangman.py
@@ -1,7 +1,6 @@
 from random import choice
 
 print(" ".join("hangman".upper()))
-print()
 
 words = ['python', 'java', 'kotlin', 'javascript']
 
@@ -9,16 +8,32 @@ word = choice(words)
 
 hidden = list('-' * len(word))
 
-for _ in range(8):
+lives = 8
+guesses = set()
+
+while lives > 0:
+    print()
     print(''.join(hidden))
+
+    if '-' not in hidden:
+        print("You guessed the word!")
+        print("You survived!")
+        break
+
     letter = input("Input a letter: ")
+
     if letter not in word:
         print("That letter doesn't appear in the word")
+        lives -= 1
+    elif letter in guesses:
+        print("No improvements")
+        lives -= 1
     else:
+        guesses.add(letter)
         ind = word.find(letter)
         while ind != -1:
             hidden[ind] = letter
             ind = word.find(letter, ind + 1)
-    print()
 
-print("Thanks for playing!")
+if lives == 0:
+    print("You lost!")
